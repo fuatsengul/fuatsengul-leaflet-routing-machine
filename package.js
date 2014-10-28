511b82f31be7f5bd2bc72a81719a610feebc6669
Fuat ŞENGÜL <fuat@fuatsengul.com.tr>
2014-10-29 01:15:59 +0200

    commit.3

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    'lib/leaflet-routing-machine',
+    'lib/leaflet-routing-machine.js',
     'lib/leaflet-routing-machine.min.js',
     'lib/leaflet-routing-machine.css']);
 
