21e85273a789cf34bb14b71fb82a5896999a92fe
Fuat ŞENGÜL <fuat@fuatsengul.com.tr>
2014-10-31 15:38:23 +0200

    1.0.6

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'fuatsengul:leaflet-routing-machine',
   summary: 'perliedman:leaflet-routing-machine porting to meteor',
-  version: '1.0.5',
+  version: '1.0.6',
   git: 'https://github.com/fuatsengul/fuatsengul-leaflet-routing-machine'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use('mrt:leaflet@0.3.8', 'client', [false, false]);
+  api.use('mrt:leaflet@0.3.8', 'client');
   api.addFiles([
     'lib/leaflet-routing-machine.js',
-    'lib/leaflet-routing-machine.css']);
+    'lib/leaflet-routing-machine.css'], 'client');
 });
 
 
@@ -19,8 +19,4 @@ Package.onTest(function(api) {
   api.use('tinytest');
   api.use('mrt:leaflet@0.3.8');
   api.use('fuatsengul:leaflet-routing-machine');
-  api.addFiles([
-    'lib/leaflet-routing-machine.js',
-    'lib/leaflet-routing-machine.min.js',
-    'lib/leaflet-routing-machine.css']);
 });
